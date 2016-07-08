e14f6ad7db6e00c7fb3b338de228559b663343e2
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-08 12:42:58 +0700

    - Add Application class

---
+++ app/src/main/java/com/appman/intern/AppManHR.java
@@ -0,0 +1,38 @@
+package com.appman.intern;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import timber.log.Timber;
+
+public class AppManHR extends Application {
+
+    public static final Gson GSON = new GsonBuilder().serializeNulls().disableHtmlEscaping().create();
+    public static final Gson GSON_PRETTY = new GsonBuilder().serializeNulls().setPrettyPrinting().disableHtmlEscaping().create();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+    }
+
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override protected void log(int priority, String tag, String message, Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+            if (priority == Log.ASSERT) {
+                Log.wtf(tag, message);
+            } else {
+                Log.println(priority, tag, message);
+            }
+        }
+    }
+}
