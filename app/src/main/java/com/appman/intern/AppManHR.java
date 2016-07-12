ccb97a0e11c6bcc1675c47baf83aa1b66312f8f1
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-12 12:18:47 +0700

    - Move files

--- app/src/main/java/com/appman/intern/AppManHR.java
+++ app/src/main/java/com/appman/intern/AppManHR.java
@@ -3,6 +3,7 @@ package com.appman.intern;
 import android.app.Application;
 import android.util.Log;
 
+import com.appman.intern.BuildConfig;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -12,6 +13,10 @@ public class AppManHR extends Application {
 
     public static final Gson GSON = new GsonBuilder().serializeNulls().disableHtmlEscaping().create();
     public static final Gson GSON_PRETTY = new GsonBuilder().serializeNulls().setPrettyPrinting().disableHtmlEscaping().create();
+    public static final String GROUP_NAME = "AppManHR";
+    public static final String ACCOUNT_TYPE = "";
+    public static final String ACCOUNT_NAME = "";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -23,7 +28,8 @@ public class AppManHR extends Application {
     }
 
     private static class CrashReportingTree extends Timber.Tree {
-        @Override protected void log(int priority, String tag, String message, Throwable t) {
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
             if (priority == Log.VERBOSE || priority == Log.DEBUG) {
                 return;
             }
---
+++ app/src/main/java/com/appman/intern/activities/MainActivity.java
@@ -0,0 +1,156 @@
+package com.appman.intern.activities;
+
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.OperationApplicationException;
+import android.database.Cursor;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.appman.intern.AppManHR;
+import com.appman.intern.R;
+import com.appman.intern.adapters.SectionsPagerAdapter;
+import com.appman.intern.databinding.MainActivityBinding;
+import com.appman.intern.models.AppContactData;
+
+import java.util.ArrayList;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity{
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private MainActivityBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+        setSupportActionBar(mBinding.toolbar);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
+        mBinding.container.setAdapter(mSectionsPagerAdapter);
+        mBinding.container.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                setToolbarTitleByIndex(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
+        mBinding.tabs.setupWithViewPager(mBinding.container);
+        mBinding.tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mBinding.container.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+    }
+
+    public void addNewContact(View view) {
+        AppContactData newContact = new AppContactData();
+        try {
+            ContentProviderResult[] results = getContentResolver().applyBatch(ContactsContract.AUTHORITY, newContact.createNewContactProvider(getGroupId()));
+            Toast.makeText(this, "Insert contact success", Toast.LENGTH_SHORT).show();
+
+            for (ContentProviderResult result : results) {
+                Log.w("insert id %s", String.valueOf(result.uri.getLastPathSegment()));
+            }
+
+        } catch (RemoteException | OperationApplicationException e) {
+            Log.e("Insert contact failed", String.valueOf(newContact), e);
+            Toast.makeText(this, "Insert contact failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setToolbarTitleByIndex(int index) {
+        mBinding.toolbarTitle.setText(mSectionsPagerAdapter.getPageTitle(index));
+    }
+
+    private String getGroupId() {
+        String groupId = ifGroup(AppManHR.GROUP_NAME);
+
+        if (groupId == null) {
+            ArrayList<ContentProviderOperation> opsGroup = new ArrayList<>();
+            opsGroup.add(ContentProviderOperation.newInsert(ContactsContract.Groups.CONTENT_URI)
+                    .withValue(ContactsContract.Groups.TITLE, AppManHR.GROUP_NAME)
+                    .withValue(ContactsContract.Groups.GROUP_VISIBLE, true)
+                    .withValue(ContactsContract.Groups.ACCOUNT_NAME, AppManHR.ACCOUNT_NAME)
+                    .withValue(ContactsContract.Groups.ACCOUNT_TYPE, AppManHR.ACCOUNT_TYPE)
+                    .build());
+            try {
+                ContentProviderResult[] results = getContentResolver().applyBatch(ContactsContract.AUTHORITY, opsGroup);
+                for (ContentProviderResult result : results) {
+                    Log.w("create group result %s", result.uri.getLastPathSegment());
+                }
+            } catch (Exception e) {
+                Log.e("create group failed", AppManHR.GROUP_NAME, e);
+            }
+
+            return ifGroup(AppManHR.GROUP_NAME);
+        } else {
+            return groupId;
+        }
+    }
+
+    private String ifGroup(String groupName) {
+        String selection = ContactsContract.Groups.DELETED + " = ? AND " + ContactsContract.Groups.GROUP_VISIBLE + " = ? AND " + ContactsContract.Groups.TITLE + " = ?";
+        String[] selectionArgs = { "0", "1", groupName };
+        Cursor cursor = getContentResolver().query(ContactsContract.Groups.CONTENT_URI, null, selection, selectionArgs, null);
+
+        if (cursor == null)
+            return null;
+
+        String id = null, title = null;
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            id = cursor.getString(cursor.getColumnIndex(ContactsContract.Groups._ID));
+            title = cursor.getString(cursor.getColumnIndex(ContactsContract.Groups.TITLE));
+        }
+
+        cursor.close();
+
+        return id;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Timber.w("Group Id %s", getGroupId());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tab, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+}
