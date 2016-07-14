66214b1eaad191de0d8ba174a8497b4f17f309c8
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-14 16:31:07 +0700

    - Change prefs name

--- app/src/main/java/com/appman/intern/AppManHRPreferences.java
+++ app/src/main/java/com/appman/intern/AppManHRPreferences.java
@@ -1,5 +1,6 @@
 package com.appman.intern;
 
+import android.app.backup.BackupManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 
@@ -7,18 +8,20 @@ import com.appman.intern.enums.Language;
 
 public class AppManHRPreferences {
 
-    private static final String PREF_NAME = "AppManHR";
-    private static final String LANGUAGE = "language";
+    public static final String PREFS = "user_preferences";
+    public static final String LANGUAGE = "language";
 
     public static Language getCurrentLanguage(Context context) {
-        SharedPreferences sharedPref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
         return Language.valueOf(sharedPref.getString(LANGUAGE, "EN"));
     }
 
     public static void setCurrentLanguage(Context context, String lang) {
-        SharedPreferences sharedPref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString(LANGUAGE, lang);
         editor.apply();
+
+        new BackupManager(context.getApplicationContext()).dataChanged();
     }
 }
