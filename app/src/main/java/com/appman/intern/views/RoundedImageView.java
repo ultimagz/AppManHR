d7a7b20276eb033dc8b7a21dc44c3e13fb1ad1a5
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-14 16:27:29 +0700

    - Edit image color config

--- app/src/main/java/com/appman/intern/views/RoundedImageView.java
+++ app/src/main/java/com/appman/intern/views/RoundedImageView.java
@@ -32,13 +32,12 @@ public class RoundedImageView extends ImageView {
             return;
         }
         Bitmap b = ((BitmapDrawable) drawable).getBitmap();
-        Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
+        Bitmap bitmap = b.copy(Config.RGB_565, true);
 
         int w = getWidth(), h = getHeight();
 
         Bitmap roundBitmap = getRoundedCroppedBitmap(bitmap, w);
         canvas.drawBitmap(roundBitmap, 0, 0, null);
-
     }
 
     public static Bitmap getRoundedCroppedBitmap(Bitmap bitmap, int radius) {
@@ -49,7 +48,7 @@ public class RoundedImageView extends ImageView {
         else
             finalBitmap = bitmap;
         Bitmap output = Bitmap.createBitmap(finalBitmap.getWidth(),
-                finalBitmap.getHeight(), Config.ARGB_8888);
+                finalBitmap.getHeight(), Config.RGB_565);
         Canvas canvas = new Canvas(output);
 
         final Paint paint = new Paint();
