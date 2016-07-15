2737df2ad3d4c9b632499a68581854c25c947da8
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-15 16:52:51 +0700

    - Add constructor with cursor

--- app/src/main/java/com/appman/intern/models/PhoneData.java
+++ app/src/main/java/com/appman/intern/models/PhoneData.java
@@ -1,8 +1,20 @@
 package com.appman.intern.models;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
 public class PhoneData extends BaseContactModel {
     int phoneType;
     String phoneNo, phoneTypeName, normalizedNumber;
+
+    public PhoneData(Cursor cursor) {
+        id = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
+        contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+        rawContactId = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.RAW_CONTACT_ID));
+        lookupKey = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.LOOKUP_KEY));
+        phoneNo = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        phoneType = cursor.getInt(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+    }
 
     public int getPhoneType() {
         return phoneType;
