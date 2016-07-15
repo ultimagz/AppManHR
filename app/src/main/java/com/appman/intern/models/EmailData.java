c955bf90f4f012426c2aba66c1e16486a60971f8
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-15 16:52:51 +0700

    - Add constructor with cursor

--- app/src/main/java/com/appman/intern/models/EmailData.java
+++ app/src/main/java/com/appman/intern/models/EmailData.java
@@ -1,8 +1,20 @@
 package com.appman.intern.models;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
 public class EmailData extends BaseContactModel {
     int emailType;
     String emailAddress, emailTypeName;
+
+    public EmailData(Cursor cursor) {
+        id = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email._ID));
+        contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.CONTACT_ID));
+        rawContactId = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.RAW_CONTACT_ID));
+        lookupKey = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.LOOKUP_KEY));
+        emailAddress = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS));
+        emailType = cursor.getInt(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE));
+    }
 
     public int getEmailType() {
         return emailType;
