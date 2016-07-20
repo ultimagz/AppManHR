5bc5a5feb923bf1348df59dd871a0333a94088cb
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-20 12:51:09 +0700

    - Change field name - Add method for insert, update, delete

--- app/src/main/java/com/appman/intern/DatabaseHelper.java
+++ app/src/main/java/com/appman/intern/DatabaseHelper.java
@@ -1,44 +1,53 @@
 package com.appman.intern;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.appman.intern.models.AppContactData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DBNAME = "DB_Contact";
     public static final String DBTABLE = "Contact";
-    public static final int DBVERSION = 1;
+    public static final int DBVERSION = 3;
 
     // Server contacts row
-    public static final String contactID = "contact_id";
-    public static final String fistNameTH = "fistName_th";
-    public static final String lastNameTH = "lastName_th";
-    public static final String nickNameTH = "nickName_th";
-    public static final String fistNameEN = "fistName_en";
-    public static final String lastNameEN = "lastName_en";
-    public static final String nickNameEN = "nickName_en";
-    public static final String position = "position";
-    public static final String email = "email";
-    public static final String mobile = "mobile";
-    public static final String workphone = "workphone";
-    public static final String lineID = "line_id";
-    public static final String updateTime = "updateTime";
+    public static final String CONTACT_ID = "contact_id";
+    public static final String FIRST_NAME_TH = "firstName_th";
+    public static final String LAST_NAME_TH = "lastName_th";
+    public static final String NICK_NAME_TH = "nickName_th";
+    public static final String FIRST_NAME_EN = "firstName_en";
+    public static final String LAST_NAME_EN = "lastName_en";
+    public static final String NICK_NAME_EN = "nickName_en";
+    public static final String POSITION = "position";
+    public static final String EMAIL = "email";
+    public static final String MOBILE = "mobile";
+    public static final String WORKPHONE = "workphone";
+    public static final String LINE_ID = "line_id";
+    public static final String UPDATE_TIME = "update_time";
+    public static final String IMAGE = "image";
 
     // Local contacts row
-    public static final String localContactID = "local_contact_id";
-    public static final String localFistNameTH = "local_fistName_th";
-    public static final String localLastNameTH = "local_lastName_th";
-    public static final String localNickNameTH = "local_nickName_th";
-    public static final String localFistNameEN = "local_fistName_en";
-    public static final String localLastNameEN = "local_lastName_en";
-    public static final String localNickNameEN = "local_nickName_en";
-    public static final String localPosition = "local_position";
-    public static final String localEmail = "local_email";
-    public static final String localMobile = "local_mobile";
-    public static final String localWorkphone = "local_workphone";
-    public static final String localLineID = "local_line";
-    public static final String localUpdateTime = "local_updateTime";
+    public static final String LOCAL_CONTACT_ID = "local_contact_id";
+    public static final String LOCAL_FIRST_NAME_TH = "local_firstName_th";
+    public static final String LOCAL_LAST_NAME_TH = "local_lastName_th";
+    public static final String LOCAL_NICK_NAME_TH = "local_nickName_th";
+    public static final String LOCAL_FIRST_NAME_EN = "local_firstName_en";
+    public static final String LOCAL_LAST_NAME_EN = "local_lastName_en";
+    public static final String LOCAL_NICK_NAME_EN = "local_nickName_en";
+    public static final String LOCAL_POSITION = "local_position";
+    public static final String LOCAL_EMAIL = "local_email";
+    public static final String LOCAL_MOBILE = "local_mobile";
+    public static final String LOCAL_WORKPHONE = "local_workphone";
+    public static final String LOCAL_LINE_ID = "local_line";
+    public static final String LOCAL_IMAGE = "local_image";
+    public static final String LOCAL_UPDATE_TIME = "local_updateTime";
 
     public DatabaseHelper(Context context) {
         super(context, DBNAME, null, DBVERSION);
@@ -47,32 +56,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String sql = String.format("CREATE TABLE %s ( ", DBTABLE) +
-                contactID + " TEXT PRIMARY KEY, " +
-                fistNameTH + " TEXT, " +
-                lastNameTH + " TEXT, " +
-                nickNameTH + " TEXT, " +
-                fistNameEN + " TEXT, " +
-                lastNameEN + " TEXT, " +
-                nickNameEN + " TEXT, " +
-                position + " TEXT, " +
-                email + " TEXT, " +
-                mobile + " TEXT, " +
-                workphone + " TEXT, " +
-                lineID + " TEXT, " +
-                updateTime + " TEXT," +
-                localContactID + " TEXT," +
-                localFistNameTH + " TEXT, " +
-                localLastNameTH + " TEXT, " +
-                localNickNameTH + " TEXT, " +
-                localFistNameEN + " TEXT, " +
-                localLastNameEN + " TEXT, " +
-                localNickNameEN + " TEXT, " +
-                localPosition + " TEXT, " +
-                localEmail + " TEXT, " +
-                localMobile + " TEXT, " +
-                localWorkphone + " TEXT, " +
-                localLineID + " TEXT, " +
-                localUpdateTime + " TEXT)";
+                CONTACT_ID + " TEXT PRIMARY KEY, " +
+                FIRST_NAME_TH + " TEXT, " +
+                LAST_NAME_TH + " TEXT, " +
+                NICK_NAME_TH + " TEXT, " +
+                FIRST_NAME_EN + " TEXT, " +
+                LAST_NAME_EN + " TEXT, " +
+                NICK_NAME_EN + " TEXT, " +
+                POSITION + " TEXT, " +
+                EMAIL + " TEXT, " +
+                MOBILE + " TEXT, " +
+                WORKPHONE + " TEXT, " +
+                LINE_ID + " TEXT, " +
+                UPDATE_TIME + " TEXT," +
+                IMAGE + " TEXT," +
+                LOCAL_CONTACT_ID + " TEXT," +
+                LOCAL_FIRST_NAME_TH + " TEXT, " +
+                LOCAL_LAST_NAME_TH + " TEXT, " +
+                LOCAL_NICK_NAME_TH + " TEXT, " +
+                LOCAL_FIRST_NAME_EN + " TEXT, " +
+                LOCAL_LAST_NAME_EN + " TEXT, " +
+                LOCAL_NICK_NAME_EN + " TEXT, " +
+                LOCAL_POSITION + " TEXT, " +
+                LOCAL_EMAIL + " TEXT, " +
+                LOCAL_MOBILE + " TEXT, " +
+                LOCAL_WORKPHONE + " TEXT, " +
+                LOCAL_LINE_ID + " TEXT, " +
+                LOCAL_IMAGE + " TEXT, " +
+                LOCAL_UPDATE_TIME + " TEXT)";
 
         db.execSQL(sql);
         Log.w("CREATE TABLE", "Create Table Successfully.");
@@ -82,6 +93,82 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + DBTABLE);
         onCreate(db);
+    }
 
+    public List<AppContactData> getContactList() {
+        SQLiteDatabase db = getReadableDatabase();
+        List<AppContactData> contactList = new ArrayList<>();
+        Cursor cursor = db.query(DatabaseHelper.DBTABLE, null, null, null, null, null, "fistName_EN ASC");
+
+        if (cursor == null)
+            return contactList;
+
+        while (cursor.moveToNext()) {
+            contactList.add(new AppContactData(cursor));
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+        Log.e("contactList", contactList.toString());
+
+        return contactList;
+    }
+
+    public AppContactData getContactById(String id) {
+        AppContactData contactData = null;
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(DatabaseHelper.DBTABLE, null, "CONTACT_ID = ?", new String[]{ id }, null, null, null);
+
+        if (cursor != null) {
+            contactData = new AppContactData(cursor);
+            cursor.close();
+        }
+
+        db.close();
+
+        return contactData;
+    }
+
+    public void insertContactList(List<AppContactData> contactList) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues insertValues;
+        for (AppContactData contactData : contactList) {
+            insertValues = contactData.createContentValues();
+            db.insertWithOnConflict(DBTABLE, null, insertValues, SQLiteDatabase.CONFLICT_REPLACE);
+        }
+
+        db.close();
+    }
+
+    public long insertContactData(AppContactData contactData) {
+        SQLiteDatabase db = getWritableDatabase();
+        String position = contactData.getPosition().replace("'", " ''");
+        ContentValues insertValues = contactData.createContentValues();
+        long result = db.insertWithOnConflict(DBTABLE, null, insertValues, SQLiteDatabase.CONFLICT_REPLACE);
+        db.close();
+
+        return result;
+    }
+
+    public int updateContactData(AppContactData contactData) {
+        SQLiteDatabase db = getWritableDatabase();
+        String where = CONTACT_ID + " = ?";
+        String[] args = { contactData.getId() };
+        ContentValues updateValues = contactData.createContentValues();
+        int result = db.update(DatabaseHelper.DBTABLE, updateValues, where, args);
+        db.close();
+
+        return result;
+    }
+
+    public int deleteContactData(AppContactData contactData) {
+        SQLiteDatabase db = getWritableDatabase();
+        String where = CONTACT_ID + " = ?";
+        String[] args = { contactData.getId() };
+
+        int result = db.delete(DatabaseHelper.DBTABLE, where, args);
+        db.close();
+
+        return result;
     }
 }
