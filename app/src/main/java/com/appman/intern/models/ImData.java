924d50f3ec160a5b8666683afd0bb3cca95d46f4
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-22 14:37:44 +0700

    - Add IM data model

---
+++ app/src/main/java/com/appman/intern/models/ImData.java
@@ -0,0 +1,27 @@
+package com.appman.intern.models;
+
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
+import org.parceler.Parcel;
+
+@Parcel
+public class ImData extends BaseContactModel {
+
+    int imType;
+    String protocol, customProtocol, label, data;
+
+    public ImData() {}
+
+    public ImData(Cursor cursor) {
+        id = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im._ID));
+        contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.CONTACT_ID));
+        rawContactId = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.RAW_CONTACT_ID));
+        lookupKey = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.LOOKUP_KEY));
+        imType = cursor.getInt(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.TYPE));
+        protocol = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.PROTOCOL));
+        customProtocol = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.CUSTOM_PROTOCOL));
+        label = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.LABEL));
+        data = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Im.DATA));
+    }
+}
