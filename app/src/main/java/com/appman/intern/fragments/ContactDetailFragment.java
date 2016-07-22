44529131bb9f23d192986bfb0d9c07defe94a8f0
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-22 15:05:27 +0700

    - Align code - Remove comment code

--- app/src/main/java/com/appman/intern/fragments/ContactDetailFragment.java
+++ app/src/main/java/com/appman/intern/fragments/ContactDetailFragment.java
@@ -59,6 +59,7 @@ public class ContactDetailFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mBinding.setContactData(mContactData);
         update(mContactData);
+
         mBinding.contactJob.setText(mContactData.getPosition());
         mBinding.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
