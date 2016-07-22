5defce1cde0ea41a883501fc2686833096275dcc
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-22 14:39:07 +0700

    - Add get/setLastExportTime

--- app/src/main/java/com/appman/intern/AppManHRPreferences.java
+++ app/src/main/java/com/appman/intern/AppManHRPreferences.java
@@ -10,6 +10,21 @@ public class AppManHRPreferences {
 
     public static final String PREFS = "user_preferences";
     public static final String LANGUAGE = "language";
+    public static final String EXPORT_TIME = "export_time";
+
+    public static String getLastExportTime(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        return sharedPref.getString(EXPORT_TIME, "");
+    }
+
+    public static void setLastExportTime(Context context, String exportTime) {
+        SharedPreferences sharedPref = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(EXPORT_TIME, exportTime);
+        editor.apply();
+
+        new BackupManager(context.getApplicationContext()).dataChanged();
+    }
 
     public static Language getCurrentLanguage(Context context) {
         SharedPreferences sharedPref = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
