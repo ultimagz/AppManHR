098c8954334e85061d8d644cb96c34ae42c8ad38
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-26 18:32:32 +0700

    - Remove comment - Remove implement filterable

--- app/src/main/java/com/appman/intern/adapters/ContactListAdapter.java
+++ app/src/main/java/com/appman/intern/adapters/ContactListAdapter.java
@@ -9,8 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
 
 import com.appman.intern.R;
 import com.appman.intern.databinding.ContactDataRowBinding;
@@ -18,6 +16,7 @@ import com.appman.intern.databinding.ContactHeaderRowBinding;
 import com.appman.intern.enums.Language;
 import com.appman.intern.fragments.ContactDetailFragment;
 import com.appman.intern.models.AppContactData;
+import com.appman.intern.models.SearchableContactData;
 import com.appman.intern.viewholders.ContactRowViewHolder;
 import com.appman.intern.viewholders.ContactSectionViewHolder;
 
@@ -27,13 +26,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ContactListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
+public class ContactListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public static final List<String> SIDE_INDEX_EN = new ArrayList<>(Arrays.asList(new String[]{
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}));
 
     final List<Integer> PHONE_TYPE_LIST = new ArrayList<>(
-            Arrays.asList(ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
+            Arrays.asList(
+                    ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
                     ContactsContract.CommonDataKinds.Phone.TYPE_HOME,
                     ContactsContract.CommonDataKinds.Phone.TYPE_WORK));
 
@@ -42,7 +42,6 @@ public class ContactListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     List<AppContactData> mFilterList;
     LayoutInflater mInflater;
     Map<String, Integer> mapIndex = new HashMap<>();
-    ItemFilter mFilter = new ItemFilter();
     Language mLanguage = Language.EN;
 
     public ContactListAdapter(FragmentActivity activity, List<AppContactData> contactList) {
@@ -111,69 +110,16 @@ public class ContactListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         return mFilterList.size();
     }
 
-//    public View getView(int position, View view, ViewGroup parent) {
-//        final AppContactData dataAtPos = mFilterList.get(position);
-//        view = dataAtPos.isHeader() ? createSessionView(dataAtPos, view, parent) : createContactView(dataAtPos, view, parent);
-//        return view;
-//    }
-
     public void setLanguage(Language language) {
         mLanguage = language;
     }
-//
-//    private View createSessionView(AppContactData dataAtPos, View convertView, ViewGroup parent) {
-//        View view = convertView;
-//        ContactSectionViewHolder viewHolder;
-//
-//        if (view == null) {
-//            view = mInflater.inflate(R.layout.contact_header_row, parent, false);
-//            viewHolder = new ContactSectionViewHolder(view);
-//            view.setTag(R.id.section_view_holder, viewHolder);
-//        } else {
-//            viewHolder = (ContactSectionViewHolder) view.getTag(R.id.section_view_holder);
-//        }
-//
-//        viewHolder.section.setText(dataAtPos.getFirstCharEn());
-//        view.setOnClickListener(null);
-//
-//        return view;
-//    }
-//
-//    private View createContactView(final AppContactData dataAtPos, View convertView, ViewGroup parent) {
-//        View view = convertView;
-//        ContactRowViewHolder viewHolder;
-//        if (view == null) {
-//            view = mInflater.inflate(R.layout.contact_data_row, parent, false);
-//            viewHolder = new ContactRowViewHolder(view);
-//            view.setTag(R.id.row_view_holder, viewHolder);
-//        } else {
-//            viewHolder = (ContactRowViewHolder) view.getTag(R.id.row_view_holder);
-//        }
-//
-//        viewHolder.title.setText(mLanguage == Language.TH ? dataAtPos.getAllNameTh() : dataAtPos.getAllNameEn());
-//        viewHolder.phoneNo.setText(dataAtPos.getMobile());
-//
-//        String base64 = dataAtPos.getImage();
-//        if (base64 != null) {
-//            byte[] imgBytes = Base64.decode(base64, Base64.DEFAULT);
-//            Glide.with(mActivity).load(imgBytes).into(viewHolder.image);
-//        }
-//
-//        view.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                showDetailFragment(dataAtPos);
-//            }
-//        });
-//
-//        return view;
-//    }
 
     private void showDetailFragment(final AppContactData dataAtPos) {
+        SearchableContactData searchDate = new SearchableContactData(dataAtPos);
         FragmentManager fragmentManager = mActivity.getSupportFragmentManager();
         fragmentManager
                 .beginTransaction()
-                .replace(R.id.main_content, ContactDetailFragment.newInstance(dataAtPos, mLanguage), "ContactDetailFragment")
+                .replace(R.id.main_content, ContactDetailFragment.newInstance(searchDate, mLanguage), "ContactDetailFragment")
                 .addToBackStack("ContactDetailFragment")
                 .commit();
     }
@@ -214,52 +160,5 @@ public class ContactListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     public int getMapIndex(String key) {
         Integer index = mapIndex.get(key);
         return index == null ? -1 : index;
-    }
-
-    @Override
-    public Filter getFilter() {
-        return mFilter;
-    }
-
-    private List<AppContactData> createFilterList(String filterString) {
-        List<AppContactData> nlist = new ArrayList<>();
-        for (AppContactData data : mOriginalList) {
-            if (checkRegionMatches(data, filterString)) {
-                nlist.add(data);
-            }
-        }
-
-        return nlist;
-    }
-
-    private boolean checkRegionMatches(AppContactData data, String filterString) {
-        int searchLength = filterString.length();
-        boolean match;
-
-        match = data.getFirstnameEn().regionMatches(true, 0, filterString, 0, searchLength);
-        match |= data.getLastnameEn().regionMatches(true, 0, filterString, 0, searchLength);
-        match |= data.getNicknameEn().regionMatches(true, 0, filterString, 0, searchLength);
-        match |= data.getFirstnameTh().regionMatches(true, 0, filterString, 0, searchLength);
-        match |= data.getLastnameTh().regionMatches(true, 0, filterString, 0, searchLength);
-        match |= data.getNicknameTh().regionMatches(true, 0, filterString, 0, searchLength);
-        match |= data.getPosition().regionMatches(true, 0, filterString, 0, searchLength);
-
-        return match;
-    }
-
-    private class ItemFilter extends Filter {
-        @Override
-        protected FilterResults performFiltering(CharSequence constraint) {
-            String filterString = constraint.toString();
-            List<AppContactData> filterlist = createFilterList(filterString);
-            mFilterList = createSectionList(filterlist);
-            return null;
-        }
-
-        @Override
-        protected void publishResults(CharSequence constraint, FilterResults results) {
-            createIndexList(mFilterList);
-            notifyDataSetChanged();
-        }
     }
 }
