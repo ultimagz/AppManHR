4712120427b68b4241a37a131ce5d643a02e6df5
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2016-07-28 15:56:48 +0700

    - Use data binding - Change request login - Check login

--- app/src/main/java/com/appman/intern/activities/LoginActivity.java
+++ app/src/main/java/com/appman/intern/activities/LoginActivity.java
@@ -1,49 +1,26 @@
 package com.appman.intern.activities;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.appman.intern.AppManHRPreferences;
 import com.appman.intern.R;
 import com.appman.intern.Utils;
+import com.appman.intern.databinding.LoginActivityBinding;
 import com.appman.intern.models.LoginModel;
 import com.facebook.stetho.okhttp.StethoInterceptor;
-import com.google.gson.Gson;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
@@ -51,38 +28,22 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
-import org.json.JSONArray;
+import java.io.IOException;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity implements Callback {
 
-
-
-    // UI references.
-    private EditText mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
-
-    public static final String MyPREFERENCES = "MyPrefs" ;
-    public static final String name = "nameKey";
-    public static final String pass = "passwordKey";
-    SharedPreferences sharedpreferences;
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient client = new OkHttpClient();
+    private LoginActivityBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
-        // Set up the login form.
-        mEmailView = (EditText) findViewById(R.id.email);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.login_activity);
 
+        mBinding.loginProgress.getIndeterminateDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY);
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        mBinding.passwordInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
@@ -93,192 +54,165 @@ public class LoginActivity extends AppCompatActivity implements Callback {
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        mBinding.signInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+        showView(false);
     }
 
     @Override
     protected void onResume() {
-        sharedpreferences=getSharedPreferences(MyPREFERENCES,
-                Context.MODE_PRIVATE);
-        if (sharedpreferences.contains(name))
-        {
-            if(sharedpreferences.contains(pass)){
-                Intent i = new Intent(this,MainActivity.class);
-                startActivity(i);
-            }
-        }
         super.onResume();
+        checkLogin();
     }
 
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
+    private void checkLogin() {
+        final boolean isLogin = AppManHRPreferences.isLogin(this);
+        mBinding.getRoot().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isLogin) gotoMainPage();
+                else showView(true);
+            }
+        }, 2000);
+    }
+
+    private void showView(boolean show) {
+        mBinding.emailInput.setVisibility(show ? View.VISIBLE : View.GONE);
+        mBinding.passwordInput.setVisibility(show ? View.VISIBLE : View.GONE);
+        mBinding.signInButton.setVisibility(show ? View.VISIBLE : View.GONE);
+        mBinding.loginProgress.setVisibility(show ? View.GONE : View.VISIBLE);
+    }
+
     private void attemptLogin() {
-
-
         // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
+        mBinding.emailInput.setError(null);
+        mBinding.passwordInput.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        String email = mBinding.emailInput.getText().toString();
+        String password = mBinding.passwordInput.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+            mBinding.passwordInput.setError(getString(R.string.error_invalid_password));
+            focusView = mBinding.passwordInput;
             cancel = true;
         }
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+            mBinding.emailInput.setError(getString(R.string.error_field_required));
+            focusView = mBinding.emailInput;
             cancel = true;
         } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
+            mBinding.emailInput.setError(getString(R.string.error_invalid_email));
+            focusView = mBinding.emailInput;
             cancel = true;
         }
 
         if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-
+            showView(false);
+            mBinding.getRoot().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    requestLogin();
+                }
+            }, 2000);
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return password.length() > 4;
     }
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-
-        SharedPreferences.Editor editor = sharedpreferences.edit();
-        String u = mEmailView.getText().toString();
-        String p = mPasswordView.getText().toString();
-        editor.putString(name, u);
-        editor.putString(pass, p);
-        editor.commit();
-        checkLogin();
-    }
-
-    public void checkLogin(){
-        Intent a = new Intent(getApplicationContext(),MainActivity.class);
-        Log.i("jsonTest",stringToJSON());
-        try {
-            post("http://192.168.1.17:8080/test2",stringToJSON());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        startActivity(a);
-    }
-
-    public String stringToJSON(){
-        String json;
+    public void requestLogin() {
         LoginModel login = new LoginModel();
-        login.setUsername(mEmailView.getText().toString());
-        login.setPassword(mPasswordView.getText().toString());
-        Gson gson = new Gson();
-        json = gson.toJson(login);
-    return json;
-    }
+        login.setUsername(mBinding.emailInput.getText().toString());
+        login.setPassword(mBinding.passwordInput.getText().toString());
 
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
-
-    OkHttpClient client = new OkHttpClient();
-
-    public void post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(JSON, Utils.GSON.toJson(login));
         Request request = new Request.Builder()
-                .url(url)
+                .url("http://hr.appmanproject.com/api/user/login")
                 .post(body)
                 .build();
 
         client.networkInterceptors().add(new StethoInterceptor());
-        client
-                .newCall(request)
-                .enqueue(this);
-    }
-
-        private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        //mEmailView.setAdapter(adapter);
+        client.newCall(request).enqueue(this);
     }
 
     @Override
-    public void onFailure(Request request, IOException e) {
+    public void onFailure(final Request request, final IOException e) {
         Log.i("Fail","Fail");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                showRequestFailDialog(e.getMessage());
+            }
+        });
     }
 
     @Override
-    public void onResponse(Response response) throws IOException {
+    public void onResponse(final Response response) throws IOException {
+        final boolean isSuccessful = response.isSuccessful();
+        final String body = response.body().toString();
         Log.i("Complete","Complete");
-        Log.i("Complete",response.body().string());
+        Log.i("Complete", body);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                checkResponse(isSuccessful, body);
+            }
+        });
+    }
+
+    private void checkResponse(boolean isSuccessful, String body) {
+        if (isSuccessful) {
+            AppManHRPreferences.setLogin(this, true);
+            mBinding.getRoot().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    gotoMainPage();
+                }
+            }, 2000);
+        } else {
+            showRequestFailDialog(body);
+        }
+    }
+
+    private void showRequestFailDialog(String message) {
+        showView(true);
+        new AlertDialog.Builder(this)
+                .setNegativeButton(R.string.button_ok, null)
+                .setCancelable(false)
+                .setMessage(message)
+                .setTitle("Request fail.")
+                .show();
+    }
+
+    private void gotoMainPage() {
+        final Intent gotoMain = new Intent(this, MainActivity.class);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(gotoMain);
+                finish();
+            }
+        });
     }
 }
 
